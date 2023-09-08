76219738f25535bf20d7cfb8ce465c79677b21ff
karthikGowdajalakadagundi <karthikjgowda722@gmail.com>
2023-09-08 20:48:42 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "car";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/car/');
+define('SITE_PATH','http://localhost/car/');
+
+define('CAR_IMAGE_SERVER_PATH',SERVER_PATH.'admin/assets/images/');
+define('CAR_IMAGE_SITE_PATH',SITE_PATH.'admin/assets/images/');
+
+if (!$conn) {
+    die("<script>alert('Connection Failed.')</script>");
+}
+
+function sendingmail($subject,$message,$reciever,$header){
+    
+    // Sending Mail
+    include('smtp/PHPMailerAutoload.php');
+    $mail=new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host="smtp.gmail.com";
+    $mail->Port=587;
+    $mail->SMTPSecure="tls";
+    $mail->SMTPAuth=true;
+    $mail->Username="dpcar01234@gmail.com";
+    $mail->Password="gjhmbxoyauflfibz";
+    $mail->SetFrom("dpcar01234@gmail.com");
+    $mail->addAddress("$reciever");
+    $mail->IsHTML(true);
+    $mail->Subject=$subject;
+    $mail->Body=$message;
+    $mail->SMTPOptions=array('ssl'=>array(
+        'verify_peer'=>false,
+        'verify_peer_name'=>false,
+        'allow_self_signed'=>false
+    ));
+ if($mail->send()){
+    $header;
+ }else{
+    echo "Mail not sent";
+ 
+ }
+                
+ }
+
+
+?>
---
+++ footer.inc.php
@@ -0,0 +1,36 @@
+<!-- social Section Starts Here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D P Car Rentals</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="css/home.css">
+
+
+</head>
+<body>
+
+<hr>
+<section class="social" >
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/profile.php?id=100041137398473/"><img src="images/fb.png"/></a>
+                </li>
+
+                <li>
+                    <a href="https://www.youtube.com/channel/UCRVFq72SrzoAHvpk6UJ4Tmg"><img src="images/youtube.png"/></a>
+                </li>
+
+                <li>
+                    <a href="https://twitter.com/KarthikJgowda5?t"><img src="images/twitter.png"/></a>
+                </li>
+        </div>
+        <div class="container text-center">
+            <p>Designed By <a href="#"><strong> Karthik </strong></p>
+    </section>
+    <!-- footer Section Ends Here -->
+    </body>
+</html>
